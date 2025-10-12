55b4b7b1f5692bc43e838fbb788d8a2d2983f717
monscao <monscao@outlook.com>
2025-10-12 14:36:33 +0800

    Updated files structure

--- src/app.tsx
+++ src/app.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import Home from './pages/Home/Home';
-import './index.css';
+import './app.scss';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
