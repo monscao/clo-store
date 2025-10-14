3116f8e921f8c7b324d8587baeb302ef424cfb11
monscao <monscao@outlook.com>
2025-10-14 12:37:19 +0800

    Added header menu and styles

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -12,9 +12,23 @@ const Home: React.FC = () => {
     <Provider store={store}>
       <div className="home">
         <header className="home-header">
-          <a href="https://connect.clo-set.com/zh">
-            <div className='home-icon'></div>
-          </a>
+          <div className='header-left'>
+            <a href="https://connect.clo-set.com/zh">
+              <div className='home-icon'></div>
+            </a>
+            <nav className="header-nav">
+              <button className="nav-button active">Store</button>
+              <button className="nav-button">Portfolio</button>
+              <button className="nav-button">Contest</button>
+              <button className="nav-button">Community</button>
+              <button className="nav-button">Application</button>
+              <button className="nav-button">Gamewear</button>
+            </nav>
+          </div>
+           <div className="header-right">
+            <button className="auth-button login-button">Login</button>
+            <button className="auth-button register-button">Register</button>
+          </div>
         </header>
         <div className="home-content">
           <div className="content-filter">
